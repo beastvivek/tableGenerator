5eecbca6401cf17f35b4649035f76568d7d15a46
Vivek Bisht <99522211+beastvivek@users.noreply.github.com>
2022-05-18 10:09:36 +0530

    Added source dir

--- generatePage.sh
+++ generatePage.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
-rm -r html 2> /dev/null
+rm -r html index.html styles.css 2> /dev/null
 mkdir html
 
 html_file_path='./html/index.html'
 csv_file_path='./data/ten.csv'
-cp './data/styles.css' './html/styles.css'
+styles_file_path='./html/styles.css'
+cp './data/styles.css' ${styles_file_path}
 
 delimiter='|'
 
@@ -13,6 +14,9 @@ echo -e $(head -n1 ${csv_file_path} | cut -d ${delimiter} -f 4- | sed 's/|/\\n/g
 
 read -p 'Enter the field according to which you want to sort  ' field
 
-node pageGenerator.js ${html_file_path} ${csv_file_path} "${field}" "${delimiter}"
+node src/pageGenerator.js ${html_file_path} ${csv_file_path} "${field}" "${delimiter}"
+
+ln -s ${html_file_path} index.html
+ln -s ${styles_file_path} styles.css
 
 open index.html
